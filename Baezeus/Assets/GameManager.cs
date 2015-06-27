f3a624f9f57acae6f293bac1d64cd3f98c5e8e75
bregord <sirbrendang@gmail.com>
2015-06-26 18:59:32 -0700

    added a few necessary scripts

--- Baezeus/Assets/GameManager.cs
+++ Baezeus/Assets/GameManager.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+
+	Plane _groundPlane;
+	public Vector3 groundOrigin = Vector3.zero;
+	public Vector3 groundNormal = Vector3.up;
+
 	// Use this for initialization
 	void Start () {
-	
+		_groundPlane = new Plane(groundNormal, groundOrigin);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void FixedUpdate(){
+
 	}
 }
---
+++ Baezeus/Assets/lighting.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lighting : MonoBehaviour {
+
+	myLine = new VectorLine ("Spline", linePoints, roadLineMaterial, 4.0f, LineType.Continuous, Joins.Fill);
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+	}
+
+	//This is where the line between two vector3 points is drawn
+
+	void DrawLine(Vector3 intersection){
+		linePoints[++lineIndex] = intersection;
+		myLine.MakeSpline (linePoints);myLine.Draw3D ();
+	}
+}
