63768212fee0fdbdcf720c95cc9ef41c3ab95c5c
unknown <weleanor@SEA-1850007992.ant.amazon.com>
2015-06-26 19:20:04 -0700

    Basic Start Menu

---
+++ Baezeus/Assets/ButtonScripts/StartMenuButtons.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenuButtons : MonoBehaviour {
+
+	public void NewGame() {
+		Application.LoadLevel("mainGameScreen");
+	}
+
+	public void ContinueGame() {
+		Application.LoadLevel ("mainGameScreen");
+	}
+
+	public void Store() {
+		Application.LoadLevel ("mainGameScreen");
+	}
+
+	public void Tutorial() {
+		Application.LoadLevel ("mainGameScreen");
+	}
+
+	public void Settings() {
+		Application.LoadLevel ("mainGameScreen");
+	}
+}
