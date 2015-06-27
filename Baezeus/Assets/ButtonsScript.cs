d9d3ec78702ca95f37e441c4b68b01e6c4cc4a06
Eleanor Wong <eleanorwong@live.ca>
2015-06-27 13:41:15 -0700

    SCENES

---
+++ Baezeus/Assets/ButtonsScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonsScript : MonoBehaviour {
+		
+		public void NewGame() {
+			Application.LoadLevel("mainGameScreen");
+		}
+		
+		public void Tutorial() {
+			Application.LoadLevel ("tutorialScreen");
+		}
+		
+		public void MainMenu() {
+			Application.LoadLevel ("startMenuScreen");
+		}
+
+}
