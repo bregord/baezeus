d4fc908846d714f994d58b1e2c0537d2394c02e9
Makoz <chenhao.john.hsu@gmail.com>
2015-06-26 22:18:41 -0700

    Critters, collisions, scripts
    Filler objects created with relev scripts
    Still need to add global var for buff icons

---
+++ Baezeus/Assets/Scripts/BaezeusAction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaezeusAction : MonoBehaviour {
+
+	public float distance;
+	public float lenFloat;
+	private float time = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter (Collider other) 
+	{
+		if (other.tag == "Critter") {
+
+			// End game script here to be triggered cuz BaeZeus is dead
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + distance* Mathf.Sin (time));
+		time += lenFloat;
+	}
+}
---
+++ Baezeus/Assets/Scripts/EndGameWall.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGameWall : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter (Collider other) 
+	{
+		if (other.tag == "Critter") {
+
+			// End game script here to be triggered cuz BaeZeus is dead
+		}
+	}
+}
