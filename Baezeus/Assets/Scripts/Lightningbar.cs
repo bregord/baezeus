230567540d146fc8ab3c58a6a97a3622847ad3e0
Eleanor Wong <eleanorwong@live.ca>
2015-06-27 07:43:31 -0700

    LightningBar Prefab

---
+++ Baezeus/Assets/Scripts/Lightningbar.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Lightningbar : MonoBehaviour {
+
+	public void fillBar(float dec) {
+		Image image = GetComponent<Image> ();
+		Image.fillAmount = dec;
+	}
+}
